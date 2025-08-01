6e1ac36987f4abb8282fb9a8513d0847072ef19d
edomcode <edenyehualashet27@gmail.com>
2025-08-01 15:22:28 +0300

    Add CRUD operations using ViewSet and Router for Book model

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+    published_date = models.DateField(default='2000-01-01')  # Add default here
 
     def __str__(self):
         return self.title
