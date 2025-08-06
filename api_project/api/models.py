7d1092d1c43af5cc0a071ff28819df4d7834d1ad
edomcode <edenyehualashet27@gmail.com>
2025-08-06 09:44:29 +0300

    Refactor settings module and fix relationship_app migration

--- api_project/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    published_date = models.DateField(default='2000-01-01')  # Add default here
-
-    def __str__(self):
-        return self.title
