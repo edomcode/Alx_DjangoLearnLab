77f22f4a8115522f39c92767b7af98aca1c79db5
edomcode <edenyehualashet27@gmail.com>
2025-08-13 20:11:20 +0300

    Add comment functionality to blog posts
    Introduces a Comment model, form, and related views for creating, updating, and deleting comments on blog posts. Updates templates to display and manage comments, modifies Post model fields, and adds migration for these changes.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,13 +1,22 @@
 from django import forms
+from .models import Comment
 
-from django import forms
-from .models import Post
-
-class PostForm(forms.ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
-        model = Post
-        fields = ['title', 'content']
+        model = Comment
+        fields = ['content'] 
 
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Write your comment here...',
+                'rows': 3
+            }),
+        }
 
-
-        
+   
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if len(content.strip()) == 0:
+            raise forms.ValidationError("Comment cannot be empty.")
+        return content
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,56 +1,87 @@
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Post
-
-
-
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
-
-
+from .models import Post, Comment
+from .forms import CommentForm
+from django.urls import reverse_lazy, reverse
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'blog/post_detail.html' 
     context_object_name = 'post'
 
-
-
-class PostCreateView(LoginRequiredMixin, CreateView):
+class PostCreateView(CreateView):
     model = Post
+    fields = ['title', 'content']  
+    template_name = 'blog/post_form.html'  
+    success_url = reverse_lazy('blog-home')  
+
+    class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+        model = Post
     fields = ['title', 'content']
     template_name = 'blog/post_form.html'
-
+    
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'pk': self.object.pk})
 
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class PostListView(ListView):
     model = Post
-    fields = ['title', 'content']
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/home.html' 
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] 
+    paginate_by = 5
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
+        post_id = self.kwargs['pk']
+        form.instance.post = get_object_or_404(Post, pk=post_id)
         form.instance.author = self.request.user
+        messages.success(self.request, 'Your comment has been posted.')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Your comment has been updated.')
         return super().form_valid(form)
 
     def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
 
 
 
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
 
     def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
