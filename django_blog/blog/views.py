e777ecb2478fd288f7d9d15c3d63252882163473
edomcode <edenyehualashet27@gmail.com>
2025-08-13 17:48:32 +0300

    Add login_required decorator import to views.py
    Imported login_required from django.contrib.auth.decorators in views.py, preparing for potential use of function-based view authentication.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
+
+
 
 
 class PostListView(ListView):
