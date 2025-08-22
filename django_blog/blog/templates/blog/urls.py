59753bd39dad4991ad6cdc5ba4a8d3eff381eaf8
edomcode <edenyehualashet27@gmail.com>
2025-08-22 17:16:29 +0300

    Set up DRF API routing with DefaultRouter and registered Post, Comment

--- django_blog/blog/templates/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   
-    path("search/", views.search_posts, name="search_posts"),
-    path("tags/<str:tag_name>/", views.posts_by_tag, name="posts_by_tag"),
-]
