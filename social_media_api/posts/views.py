33d208258978d3dab13f156af3289e26e63e2adf
edomcode <edenyehualashet27@gmail.com>
2025-08-24 22:04:35 +0300

    Add like/unlike views with Like model and notification creation; finalize Post and Comment viewsets

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, permissions, status, generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.contrib.contenttypes.models import ContentType
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.views import APIView
 
 from accounts.models import CustomUser
 from notifications.models import Notification
@@ -12,14 +14,12 @@ from .serializers import PostSerializer, CommentSerializer
 @permission_classes([permissions.IsAuthenticated])
 def like_post(request, pk):
     post = generics.get_object_or_404(Post, pk=pk)
-    user = request.user
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
 
-    like, created = Like.objects.get_or_create(user=user, post=post)
-
-    if created and post.author != user:
+    if created and post.author != request.user:
         Notification.objects.create(
             recipient=post.author,
-            actor=user,
+            actor=request.user,
             verb='liked your post',
             content_type=ContentType.objects.get_for_model(post),
             object_id=post.id
@@ -31,17 +31,29 @@ def like_post(request, pk):
 @permission_classes([permissions.IsAuthenticated])
 def unlike_post(request, pk):
     post = generics.get_object_or_404(Post, pk=pk)
-    user = request.user
-
-    Like.objects.filter(user=user, post=post).delete()
-
+    Like.objects.filter(user=request.user, post=post).delete()
     return Response({'status': 'post unliked'}, status=status.HTTP_200_OK)
 
-# Keep your existing viewsets below
+class FeedView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        following_users = user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS or obj.author == request.user
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -49,7 +61,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
