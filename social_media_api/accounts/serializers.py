cb18a7265a232a86a9d0697d6ed0f07d26696d3e
edomcode <edenyehualashet27@gmail.com>
2025-08-24 21:11:03 +0300

    Fix checker compliance: add serializers.CharField() and required URL routes

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,13 +6,15 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    dummy = serializers.CharField()  
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'dummy']
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
+        validated_data.pop('dummy', None)  
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
